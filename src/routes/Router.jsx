c20568090bf17f0647eeb1789ed21224328b8081
hayoung <105140066+haayyoung@users.noreply.github.com>
2026-07-18 20:02:19 +0900

    [feat] 방 참여 페이지 구현 (#3)

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,27 +1,32 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from 'react-router-dom'
 
-import LandingPage from "../pages/LandingPage";
-import CreateRoomPage from "../pages/CreateRoomPage";
-import GamePage from "../pages/GamePage";
-import ResultPage from "../pages/ResultPage";
+import LandingPage from '../pages/LandingPage'
+import CreateRoomPage from '../pages/CreateRoomPage'
+import JoinRoomPage from '../pages/JoinRoomPage'
+import GamePage from '../pages/GamePage'
+import ResultPage from '../pages/ResultPage'
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <LandingPage />,
   },
   {
-    path: "/rooms/create",
+    path: '/rooms/create',
     element: <CreateRoomPage />,
   },
   {
-    path: "/rooms/:entryCode/game",
+    path: '/join-room',
+    element: <JoinRoomPage />,
+  },
+  {
+    path: '/rooms/:entryCode/game',
     element: <GamePage />,
   },
   {
-    path: "/rooms/:entryCode/result",
+    path: '/rooms/:entryCode/result',
     element: <ResultPage />,
   },
-]);
+])
 
-export default router;
+export default router
